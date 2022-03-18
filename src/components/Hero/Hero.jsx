6fff69aca21e0e5b95992e12d77e0b5bf93b4cc3
Chi Ming Yip <cmyip99@gmail.com>
2022-03-18 19:54:13 +1100

    Adjusted title on hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -23,7 +23,7 @@ const Header = () => {
 
   return (
     <section id="hero" className="jumbotron">
-      <Container>
+      <Container className="hero-container">
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
           <h1 className="hero-title">
             {title || 'Hi, my name is'}{' '}
